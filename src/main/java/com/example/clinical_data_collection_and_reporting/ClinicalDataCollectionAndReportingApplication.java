991bdbf25184390eef81611f372a349421ab7d56
decagon <godsonjonathan39@gmail.com>
2022-03-05 23:13:10 +0100

    Created Boiler Plate

---
+++ src/main/java/com/example/clinical_data_collection_and_reporting/ClinicalDataCollectionAndReportingApplication.java
@@ -0,0 +1,13 @@
+package com.example.clinical_data_collection_and_reporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalDataCollectionAndReportingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicalDataCollectionAndReportingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clinical_data_collection_and_reporting/ClinicalDataCollectionAndReportingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clinical_data_collection_and_reporting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalDataCollectionAndReportingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
