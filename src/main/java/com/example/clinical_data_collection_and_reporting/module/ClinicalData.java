c4232cfd2c9ebd046b6885d91d54756091d9c2c7
decagon <godsonjonathan39@gmail.com>
2022-03-06 01:57:16 +0100

    Created Tables and their Relationship

--- src/main/java/com/example/clinical_data_collection_and_reporting/module/ClinicalData.java
+++ src/main/java/com/example/clinical_data_collection_and_reporting/module/ClinicalData.java
@@ -1,4 +1,30 @@
 package com.example.clinical_data_collection_and_reporting.module;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ClinicalData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Long patient_id;
+    private String component_name;
+    private String component_value;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date measured_date_time;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="patient_id", referencedColumnName = "Id")
+    private Patient patient;
+
 }
--- src/main/java/com/example/clinical_data_collection_and_reporting/module/Patient.java
+++ src/main/java/com/example/clinical_data_collection_and_reporting/module/Patient.java
@@ -1,4 +1,28 @@
 package com.example.clinical_data_collection_and_reporting.module;
 
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GeneratorType;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
+    @NotNull
+    private int age;
 }
